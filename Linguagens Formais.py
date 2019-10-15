701b0f83240875b2c8de45cf4f963e733e9d23cc
Vinicius Cavichioli <vinicavi@hotmail.com>
2019-10-15 19:39:46 -0300

    atulizado 2

--- Linguagens Formais.py
+++ Linguagens Formais.py
@@ -9,7 +9,7 @@ data1 = requests.get ('http://ulbra-to.br/espaco-academico/2011/02/07/Ouvidoria'
 phones = re.findall (r'[0-9]{4}\-[0-9]{4}', data.text)
 emails = re.findall(r'([\d\w\.]+@[\d\w\.\-]+\.\w+)', data.text)
 
-phones1 = re.findall (r'(?:[2-8]|9[1-9])[0-9]{3}\-[0-9]{4}', data1.text)
+phones1 = re.findall (r'[0-9]{4}\-[0-9]{4}', data1.text)
 emails1 = re.findall(r'([\d\w\.]+@[\d\w\.\-]+\.\w+)', data1.text)
 
 #Prints
